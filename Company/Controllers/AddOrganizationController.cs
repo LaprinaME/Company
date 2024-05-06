5350621c379d1ebc864bdbd9e7f194af939e8a03
LaprinaME <112739124+LaprinaME@users.noreply.github.com>
2024-05-06 14:02:36 +0700

    ....

--- Company/Controllers/AddOrganizationController.cs
+++ Company/Controllers/AddOrganizationController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Company.Models;
+using System.Threading.Tasks;
 using Company.DataContext;
+using Company.ViewModels;
 
 namespace Company.Controllers
 {
@@ -16,20 +18,37 @@ namespace Company.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var viewModel = new AddOrganizationViewModel(); // Создание экземпляра ViewModel
+            return View(viewModel); // Передача ViewModel в представление
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Название_организации,ИНН,КПП,ОГРН,Дата_закрытия")] Организации организации)
+        public async Task<IActionResult> AddOrganization(AddOrganizationViewModel viewModel)
         {
             if (ModelState.IsValid)
             {
+                // Маппинг данных из ViewModel в сущность Организации
+                var организации = new Организации
+                {
+                    Название_организации = viewModel.Название_организации,
+                    ИНН = viewModel.ИНН,
+                    КПП = viewModel.КПП,
+                    ОГРН = viewModel.ОГРН,
+                    Код = viewModel.Код,
+                    Тип_организации = viewModel.Тип_организации,
+                    Дата_закрытия = viewModel.Дата_закрытия,
+                    Код_родительской_организации = viewModel.Код_родительской_организации,
+                    Код_организации = viewModel.Код_организации,
+                    Код_статуса = viewModel.Код_статуса
+                };
+
                 _context.Add(организации);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index)); // Предполагается, что у вас есть метод Index для отображения списка организаций
             }
-            return View(организации);
+            // Возвращение представления Index в случае невалидных данных
+            return View("Index", viewModel);
         }
     }
 }
